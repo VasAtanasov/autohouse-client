2615f9f69b94252e152659c39787a653910618fe
VasAtanasov <vas.atanasov@gmail.com>
2020-02-08 17:27:14 +0000

    initialized

--- src/components/Home/Home.styles.js
+++ src/components/Home/Home.styles.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 const StyledContainer = styled.div`
-    color: blue;
+    color: ${props => props.theme.colors.lightPurpleTwo};
+    background-color: ${props => props.theme.colors.cloudGray};
 `;
 
 export { StyledContainer };
