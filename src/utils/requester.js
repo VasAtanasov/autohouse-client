66be11c8773f31f155448d2c3ad072c7958ad848
VasAtanasov <vas.atanasov@gmail.com>
2020-08-08 14:03:38 +0300

    changed base url to heroku api

--- src/utils/requester.js
+++ src/utils/requester.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8007/api';
+const API_BASE_URL = 'https://autohouse-server.herokuapp.com/api';
 const instance = axios.create({
   baseURL: API_BASE_URL,
 });
